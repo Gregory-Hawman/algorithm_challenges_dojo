9c90ff7925bcf9a38167abf6c3e02735d8b69161
Gregory Hawman <g.hawman@gmail.com>
2021-11-16 13:25:20 -0500

    set up chapter 5 problems

---
+++ chapter 5/02_listContains.js
@@ -0,0 +1,3 @@
+// Sam thinks Tad might be somewhere in a very long line waiting to
+// attend the Superman movie. Given a ListNode pointer and a val,
+// return whether val is found in any node in the list.
---
+++ chapter 5/04_listFront.js
@@ -0,0 +1,4 @@
+// Finally, Tad and Sam reach the front of the line to get movie tickets.
+// Oh no – only one seat remains! Who was earlier in line: Tad or Sam?
+// Return the value (not the node) at the head of the list. If list is empty,
+// return null.
