8601de548b1f117d15eec24741e8383e4fc8ed49
Gregory Hawman <g.hawman@gmail.com>
2022-01-10 16:06:09 -0500

    finished remove min and remove last min

--- chapter 6/08_queueRemoveLastMin.js
+++ chapter 6/08_queueRemoveLastMin.js
@@ -1,11 +1,3 @@
-// [] SLQueue: Remove Minimums
-// Create a standalone function to remove an SLQueue’s lowest value,
-// otherwise leaving values in the same sequence. Use only local
-// variables; allocate no other objects. Remove all duplicates of this
-// value.
-// Bonus: Remove only the last minimum value. Convert [7,2,5,2,4] to
-// [7,2,5,4].
-
 class Node {
     constructor(val) {
         this.val = val
@@ -14,7 +6,7 @@ class Node {
 }
 
 class SLQueue {
-    constructor() {
+    constructor(){
         this.head = null
         this.tail = null
     }
@@ -31,33 +23,23 @@ class SLQueue {
     }
 }
 
-function removeAllMins(queue) {
+function removeLastMins(queue) {
     let min = queue.head.val
     let minLoc = [0]
     let queueIndex = 0
     let currentNode = queue.head
-    
-    // first loop, looking for the locations of mins
+    let tailMin = false
+
     while (currentNode.next !== queue.tail.next) {
         currentNode = currentNode.next
         queueIndex++
-        if(currentNode.val < min){
+        if(currentNode.val <= min){
             min = currentNode.val
             minLoc = []
             minLoc.push(queueIndex)
-        } else if(currentNode.val === min){
-            minLoc.push(queueIndex)
         }
     }
-
-    // check the tail if it is min or includes min
-    let tailMin = false
-    if(queue.tail.val < min){
-        min = currentNode.val
-        minLoc = []
-        minLoc.push(queueIndex)
-        tailMin = true
-    } else if (queue.tail.val === min) {
+    if(queue.tail.val <= min){
         tailMin = true
     }
 
@@ -65,14 +47,12 @@ function removeAllMins(queue) {
     currentNode = queue.head
     let newTail = queueIndex - 1
     queueIndex = 0
- 
-    // check if head or tail are mins
-    if (queue.head.val === min) {
+
+    if (minLoc[0] === 0) {
         queue.head = queue.head.next
         minLoc.shift()
     }
-
-    if (tailMin) {
+    if(tailMin) {
         while(minLocIndex < 1){
             if (queueIndex === newTail){
                 queue.tail = currentNode
@@ -83,25 +63,22 @@ function removeAllMins(queue) {
             queueIndex ++
         }
     }
-
+    
     currentNode = queue.head
     queueIndex = 0
     minLocIndex = 0
 
     while (minLoc.length > 0) {
-        if(queueIndex === minLoc[minLocIndex] - 1){
+        if(queueIndex === minLoc[minLocIndex] - 1) {
             currentNode.next = currentNode.next.next
-            queueIndex = 0
-            currentNode = queue.head
-            minLocIndex = 0
-            minLoc.shift()
-            continue
+            queueIndex ++
+            minLocIndex++
+            minLoc.pop()
         }
         currentNode = currentNode.next
-        queueIndex++
+        queueIndex ++
     }
 
-    // Get all the values from a queue
     currentNode = queue.head
     let queueValues = []
     while (currentNode.next !== queue.tail.next) {
@@ -113,9 +90,9 @@ function removeAllMins(queue) {
 }
 
 const myQueue = new SLQueue();
+myQueue.enqueue(2)
 myQueue.enqueue(7)
 myQueue.enqueue(5)
 myQueue.enqueue(2)
-myQueue.enqueue(2)
-myQueue.enqueue(4)
-console.log(removeAllMins(myQueue))
+myQueue.enqueue(3)
+console.log(removeLastMins(myQueue))
